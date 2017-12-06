af3e3e5d9bfceb0c17713695df3853c553941a5f
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2017-12-06 16:04:46 +0100

    Added the mail js file for calulation.

--- WhitakersRegression/js/main.js
+++ WhitakersRegression/js/main.js
@@ -1,1 +1,71 @@
+$(document).ready(function () {
+
+    function linearRegression(y, x) {
+        var lr = {};
+        var n = y.length;
+        var sum_x = 0;
+        var sum_y = 0;
+        var sum_xy = 0;
+        var sum_xx = 0;
+        var sum_yy = 0;
+
+        for (var i = 0; i < y.length; i++) {
+
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_xy += (x[i] * y[i]);
+            sum_xx += (x[i] * x[i]);
+            sum_yy += (y[i] * y[i]);
+        }
+
+        lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+        lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
+        lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
+
+        return lr;
+    }
+
+    $('#AddBtn').click(function () {
+
+        $("<input type='text' value='' class='form-control dataX'/>")
+            //.attr("id", "myfieldid")
+            //.attr("name", "myfieldid")
+            .appendTo(".datacoloumX");
+
+        $("<input type='text' value='' class='form-control dataYt'/>")
+            //.attr("id", "myfieldid")
+            //.attr("name", "myfieldid")
+            .appendTo(".datacoloumYt");
+
+        $("<input type='text' value='' class='form-control dataYm'/>")
+            //.attr("id", "myfieldid")
+            //.attr("name", "myfieldid")
+            .appendTo(".datacoloumYm");
+
+
+
+    });
+
+
+    $('#Exec').click(function () {
+
+        var valuesX = [];
+        $('.dataX').each(function () {
+            values.push($(this).val());
+        });
+
+        var valuesYt = [];
+        $('.dataYt').each(function () {
+            values.push($(this).val());
+        });
+
+        var calculatedItems = linearRegression(valuesX, valuesYt);
+        
+
+        alert(calculatedItems);
+
+
+    });
+
+});
 
