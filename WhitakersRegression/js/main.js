58c5199114512b57aa276e5d53c3e696eb3a6dca
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2017-12-09 10:42:08 +0100

    debugging version.

--- WhitakersRegression/js/main.js
+++ WhitakersRegression/js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
 
     function linearRegression(y, x) {
         var lr = {};
+        //var m = x.length;
+        //alert("x length: " + m);
         var n = y.length;
+        //alert("y length:" + n);
         var sum_x = 0;
         var sum_y = 0;
         var sum_xy = 0;
@@ -11,6 +14,8 @@ $(document).ready(function () {
 
         for (var i = 0; i < y.length; i++) {
 
+            alert("element x: " + i + " = " + x[i]);
+            alert("element y: " + i + " = " + y[i]);
             sum_x += x[i];
             sum_y += y[i];
             sum_xy += (x[i] * y[i]);
@@ -20,9 +25,14 @@ $(document).ready(function () {
 
         //a
         lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+        var test = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+        alert("a: " + test);
+        alert("slope: " + lr['slope']);
         //b
         lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
         lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
+
+
 
         return lr;
     }
@@ -55,14 +65,15 @@ $(document).ready(function () {
         $('.dataX').each(function () {
             valuesX.push($(this).val());
         });
-
+        
         var valuesYt = [];
         $('.dataYt').each(function () {
             valuesYt.push($(this).val());
         });
+        
 
-        var calculatedItems = linearRegression(valuesX, valuesYt);
-
+        var calculatedItems = linearRegression(valuesYt, valuesX);
+        
         //Disable ExcuteResolve Button
         $('#ExecResolve').removeClass('btn-primary');
         $('#ExecResolve').addClass('btn-default');
@@ -91,34 +102,34 @@ $(document).ready(function () {
         $('.dataXCalced').prop('readonly', false);
 
         //Insert resolved values:
-        $("<strong>Obtained value's:<br> a: " + calculatedItems['slope'] + " b: " + calculatedItems['intercept'] + "</strong>").appendTo(".resolve-output");
+        //$("<strong>Obtained value's:<br> a: " + calculatedItems['slope'] + " b: " + calculatedItems['intercept'] + "</strong>").appendTo(".resolve-output");
 
-
-
-        //alert(calculatedItems['slope']);
-
-
-    });
-
-    $('#ExecCalcX').click(function () {
-
-        var valuesX = [];
-        $('.dataX').each(function () {
-            valuesX.push($(this).val());
-        });
-
-        var valuesYt = [];
-        $('.dataYt').each(function () {
-            valuesYt.push($(this).val());
-        });
-
-        var calculatedItems = linearRegression(valuesX, valuesYt);
 
 
         alert(calculatedItems['slope']);
 
 
     });
+
+    //$('#ExecCalcX').click(function () {
+
+    //    var valuesX = [];
+    //    $('.dataX').each(function () {
+    //        valuesX.push($(this).val());
+    //    });
+
+    //    var valuesYt = [];
+    //    $('.dataYt').each(function () {
+    //        valuesYt.push($(this).val());
+    //    });
+
+    //    var calculatedItems = linearRegression(valuesYt, valuesX);
+
+
+    //    alert(calculatedItems['slope']);
+
+
+    //});
 
 });
 
